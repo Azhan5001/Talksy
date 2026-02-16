67daf5b6cd4ab7f54d59567ab70ef23369dfc2b3
Azhan5001 <183731468+Azhan5001@users.noreply.github.com>
2026-02-16 11:58:47 +0800

    ready for deployment

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,11 @@
 import {Router} from "express";
+import {login, register} from "../controllers/auth.controller.js"
 
 const authRouter  = Router();
 
 authRouter.get("/", (req, res) => {})
-authRouter.post("/login", (req, res) => {})
-authRouter.post("/register", (req, res) => {})
+authRouter.post("/login", login)
+authRouter.post("/register", register)
 authRouter.post("/logout", (req, res) => {})
 
 export default authRouter;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,30 @@
 import express from 'express';
-import {PORT} from "../config/env.js";
+import dotenv from 'dotenv';
+import path from 'path';
+
 import authRouter from "./routes/auth.route.js";
 import messageRouter from "./routes/messages.route.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 const app = express()
 
 app.use(express.json())
+const __dirname = path.resolve();
 
 app.use('/api/auth', authRouter);
 app.use('/api/messages', messageRouter);
+
+//make ready for deployment
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get('*', (_, res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+}
 
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}:`));
